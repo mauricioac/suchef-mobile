d04fb7bd19f6fe89eb42bffba20b669400e7d937
Maurício André Cinelli <mauricio.andre.cinelli@gmail.com>
2017-11-24 22:10:43 -0200

    Criando tabs

---
+++ app/src/main/java/com/suchef/suchef/TabProdutos.java
@@ -0,0 +1,28 @@
+package com.suchef.suchef;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TabProdutos extends Fragment{
+
+    public TabProdutos() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.tab_produtos, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/suchef/suchef/TabRestaurante.java
@@ -0,0 +1,28 @@
+package com.suchef.suchef;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TabRestaurante extends Fragment{
+
+    public TabRestaurante() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.tab_restaurante, container, false);
+    }
+
+}
