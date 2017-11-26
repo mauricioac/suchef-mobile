9977a809f05d702a3b9ee060f12f2e23fca2d1ad
Maurício André Cinelli <mauricio.cinelli@bitzesty.com>
2017-11-26 14:56:52 -0200

    Começando compartilhar e pedidos

--- app/src/main/java/com/suchef/suchef/Restaurante.java
+++ app/src/main/java/com/suchef/suchef/Restaurante.java
@@ -1,18 +1,26 @@
 package com.suchef.suchef;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.widget.FrameLayout;
 import android.widget.ListAdapter;
+import android.support.v7.widget.ShareActionProvider;
 import android.widget.SimpleAdapter;
 
 import com.android.volley.AuthFailureError;
@@ -37,6 +45,8 @@ public class Restaurante extends AppCompatActivity {
     private ViewPager viewPager;
     HashMap<String, String> filial;
     TabProdutos tabProdutos;
+    private ShareActionProvider mShareActionProvider;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +56,8 @@ public class Restaurante extends AppCompatActivity {
         sharedPref = this.getSharedPreferences(this.getString(R.string.app_name), Context.MODE_PRIVATE);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
 
         filial = (HashMap<String, String>) getIntent().getSerializableExtra("filial");
 
@@ -148,4 +160,49 @@ public class Restaurante extends AppCompatActivity {
             return mFragmentTitleList.get(position);
         }
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_restaurante, menu);
+
+        // Locate MenuItem with ShareActionProvider
+        final MenuItem item = menu.findItem(R.id.action_share);
+        View v =  MenuItemCompat.getActionView(item);
+        v.setClickable(true);
+        System.out.println(item);
+
+//        v.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                onOptionsItemSelected(item);
+//            }
+//        });
+
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+
+        return true;
+    }
+
+    private void setShareIntent(Intent shareIntent) {
+        startActivity(Intent.createChooser(shareIntent, "Enviar para"));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // action with ID action_refresh was selected
+            case R.id.action_share:
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                setShareIntent(intent);
+                break;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected((item));
+    }
+
 }
--- app/src/main/java/com/suchef/suchef/TabProdutos.java
+++ app/src/main/java/com/suchef/suchef/TabProdutos.java
@@ -1,13 +1,20 @@
 package com.suchef.suchef;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.LayoutRes;
+import android.app.FragmentManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.NumberPicker;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
@@ -15,7 +22,7 @@ import java.util.HashMap;
 import java.util.List;
 
 
-public class TabProdutos extends Fragment{
+public class TabProdutos extends Fragment {
 
     Button finalizarPedido;
     ListView listaProdutos;
@@ -42,14 +49,53 @@ public class TabProdutos extends Fragment{
     }
 
     public void atualizaProdutos(List<HashMap<String, String>> produtos) {
-        ListAdapter adapter = new SimpleAdapter(getActivity().getApplicationContext(), produtos, R.layout.item_produto,
-                new String[] { "preco", "nome", "descricao" },
+
+        for (HashMap<String, String> produto : produtos) {
+            produto.put("preco_formatado", "R$ " + produto.get("preco"));
+        }
+
+        ListAdapter adapter = new ProdutosListAdapter(getActivity().getFragmentManager(), getActivity().getApplicationContext(), produtos, R.layout.item_produto,
+                new String[] { "preco_formatado", "nome", "descricao" },
                 new int[] { R.id.precoProduto, R.id.nomeProduto, R.id.descricaoProduto });
 
 
-        System.out.println("LOLZ");
-        System.out.println(produtos);
+        listaProdutos.setClickable(false);
         listaProdutos.setAdapter(adapter);
     }
 
+}
+
+class ProdutosListAdapter extends SimpleAdapter {
+    FragmentManager fm;
+
+    public ProdutosListAdapter(FragmentManager manager, Context context, java.util.List<? extends java.util.Map<java.lang.String,?>> data, @LayoutRes int layout_id, String []keys, @IdRes int []ids) {
+        super(context, data, layout_id, keys, ids);
+
+        this.fm = manager;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = super.getView(position, convertView, parent);
+
+        final EditText quantidade = (EditText) v.findViewById(R.id.txtQuantidade);
+        quantidade.setFocusable(false);
+
+        final NumberPickerDialog np = new NumberPickerDialog();
+        np.setValueChangeListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                quantidade.setText(String.valueOf(i));
+            }
+        });
+
+        quantidade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                np.show(fm, "time picker");
+            }
+        });
+
+        return v;
+    }
 }
